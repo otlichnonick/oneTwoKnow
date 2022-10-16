6cf090ddd277639d47f014d24d83b1a40ff647ec
anton <otlichnonick@yandex.ru>
2022-10-16 11:09:57 +0200

    Initial Commit

---
+++ oneTwoKnow/oneTwoKnowApp.swift
@@ -0,0 +1,17 @@
+//
+//  oneTwoKnowApp.swift
+//  oneTwoKnow
+//
+//  Created by Anton Agafonov on 16.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct oneTwoKnowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
